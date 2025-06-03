781465c2d1a09f8b51b2257054ab6ccc6923cbf8
Teodor Hristov <justteshi@gmail.com>
2025-06-03 21:00:34 +0300

    settings for prod

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-cmbt(u8-6^2gl_^q81unp#l&4d9c0_c=+a@@i09uk1m5dxh!qg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['solarcargo.commitandpray.com', 'localhost']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
