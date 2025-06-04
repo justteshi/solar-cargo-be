b8e69878eda0a856b18ba915c36fa7b4b0bc8191
Teodor Hristov <justteshi@gmail.com>
2025-06-04 17:29:24 +0300

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', include('reports.urls')),  # <-- include your app urls here
+    path('', include('reports.urls')),  # <-- include your app urls here
 
     # Swagger UI:
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
