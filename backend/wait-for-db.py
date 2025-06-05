bac45067ff4f53ffffe53f8effe9aa9dc908eb73
Teodor Hristov <thristov@abrites.com>
2025-06-05 11:09:19 +0300

    Add healthcheck for db container, merge for-prod into main and add wait-for-db.py script

---
+++ backend/wait-for-db.py
@@ -0,0 +1,19 @@
+import time
+import psycopg2
+import os
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            dbname=os.environ["POSTGRES_DB"],
+            user=os.environ["POSTGRES_USER"],
+            password=os.environ["POSTGRES_PASSWORD"],
+            host="db",
+            port="5432",
+        )
+        conn.close()
+        print("PostgreSQL is ready.")
+        break
+    except psycopg2.OperationalError:
+        print("Waiting for PostgreSQL...")
+        time.sleep(2)
