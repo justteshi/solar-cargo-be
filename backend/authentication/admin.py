709b7c5937fe897d5284a7d870d06722aa6ba5e6
YordanovND <niki.job@abv.bg>
2025-06-09 20:58:13 +0300

    Migrate to JWT Token, Fix Rate Limiter, Add Roles, Order Delivery reports by Created_at desc

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,4 +1,37 @@
-from django.contrib import admin
-from .models import UserAPIKey
+# authentication/admin.py
 
-admin.site.register(UserAPIKey)
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import UserProfile
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(DefaultUserAdmin):
+    inlines = (UserProfileInline,)
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username',
+                'email',
+                'password1',
+                'password2',
+            ),
+        }),
+    )
+
+    # make sure list_display still shows email & role
+    list_display = ('username', 'email', 'get_role', 'is_staff')
+    list_select_related = ('profile',)
+
+    def get_role(self, obj):
+        return obj.profile.role
+    get_role.short_description = 'Role'
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,5 +1,30 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField(
+        help_text="Valid refresh token to blacklist"
+    )
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['userName'] = user.username
+        token['userRole'] = getattr(user, 'role', None)
+        return token
+
+class TokenResponseSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+    access  = serializers.CharField()
+
+class AccessSerializer(serializers.Serializer):
+    access = serializers.CharField()
+
+class LogoutResponseSerializer(serializers.Serializer):
+    detail = serializers.CharField()
