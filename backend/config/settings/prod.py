3765c7daf942db9f6a1817805a495265c4f5cd90
Nikolay Todorov <nik40.999@gmail.com>
2025-06-11 10:09:37 +0100

    add S3 file storage for persisting media files

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,4 +1,8 @@
 from .base import *
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 DEBUG = False
 
@@ -26,3 +30,25 @@ CSRF_COOKIE_SECURE = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# S3 storage for user-uploaded images
+
+INSTALLED_APPS += ['storages']
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STORAGES = {
+    # Media file (image) management
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+
+    # CSS and JS file management
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+}
