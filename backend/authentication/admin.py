7fc22a9acfe90a3c4c68546f628bbd3c67279e5a
YordanovND <niki.job@abv.bg>
2025-06-11 20:19:12 +0300

    Fix admin panel

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,5 +1,3 @@
-# authentication/admin.py
-
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
@@ -25,12 +23,15 @@ class CustomUserAdmin(DefaultUserAdmin):
         }),
     )
 
-    # make sure list_display still shows email & role
     list_display = ('username', 'email', 'get_role', 'is_staff')
     list_select_related = ('profile',)
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('profile')
+
     def get_role(self, obj):
-        return obj.profile.role
+        return getattr(obj.profile, 'role', '-') if hasattr(obj, 'profile') else '-'
     get_role.short_description = 'Role'
 
 admin.site.unregister(User)
